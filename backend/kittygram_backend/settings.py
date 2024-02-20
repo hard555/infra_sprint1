e5d527b766cd9bface0f1ebd176641bd45e82ed4
hard555 <collaps555@gmail.com>
2024-02-20 14:59:23 +0000

    www

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['84.201.161.150', '127.0.0.1', 'localhost', 'sprint15.ddns.net']
 
